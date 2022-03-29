f4753e34ff6cc3f687ff8eae3a2dc3c716bac36d
Megan Silva <megansilva@Megans-MacBook-Pro.local>
2022-03-29 13:37:39 -0400

    adding functions for no answer, four options and three options selected

--- assets/script.js
+++ assets/script.js
@@ -8,8 +8,6 @@
 // Ends with password being generated!
 
 
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -20,8 +18,8 @@ var confirmNumber;
 var confirmSpecial;
 var userSelection;
 
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
@@ -42,7 +40,7 @@ function generatePassword() {
   passwordLength = prompt ("How many characters would you like your password to contain? Choose between 8 and 128 characters.");
   console.log("Passwork length" + passwordLength);
 
-  if (passwordLength) {
+  if (!passwordLength) {
     alert ("Required value");
 
   } else if (passwordLength < 8 && passwordLength > 128); {
@@ -60,6 +58,42 @@ function generatePassword() {
       console.log = ("Special characters" + confirmSpecial);
   };
 }
+
+// Receiving no answer
+if (!confirmUpper && !confirmLower && !confirmNumber && !confirmSpecial) {
+  userSelection = alert ("You MUST choose a criteria for your password.");
+
+// Receiving all four options
+} else if (confirmUpper && confirmLower && confirmNumber && confirmSpecial) {
+  userSelection = upperCase.concat(lowerCase, number, specialChar);
+    console.log(userSelection);
+  
+// Receiving three opetions:
+//Upper && lower && number
+//Upper && lower && special characters
+//Upper && number && special characters
+//Lower && number && special characters 
+} else if (confirmUpper && confirmLower && confirmNumber) {
+  userSelection = upperCase.concat(lowerCase, number);
+    console.log(userSelection);
+} else if (confirmUpper && confirmLower && confirmSpecial) {
+  userSelection = upperCase.concat(lowerCase, specialChar);
+    consolelog(userSelection);
+} else if (confirmUpper && confirmNumber && confirmSpecial) {
+  userSelection = upperCase.concat(number, specialChar);
+    console.log(userSelection);
+} else if (confirmLower && confirmNumber && confirmSpecial) {
+  userSelection = lowerCase.concat(number, specialChar);
+    console.log(userSelection);
+
+// Receiving two options:
+//Upper && lower
+//Upper && number
+//Upper && special characters
+//Loweer && number
+//Lower && special characters
+//Number && special characters
+} else if ()
 
 
 
