b910541e4dbd6a3f7eac9da42aed9e34fd21864f
Megan Silva <megansilva@Megans-MacBook-Pro.local>
2022-03-29 12:23:50 -0400

    adding valuables to the requirements for password

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,29 @@
 // Assignment code here
 
 // How many charcters would you like your password to contain?
+// Click OK to confirm including uppercase charcters?
+// Click OK to confirm including lowercase charcters?
 // Click OK to confirm including special charcters?
 // Click OK to confirm including numeric charcters?
-// Click OK to confirm including lowercase charcters?
-// Click OK to confirm including uppercase charcters?
 // Ends with password being generated!
 
-var enter;
+
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+var passwordLength;
+var confirmUpper;
+var confirmLower;
+var confirmNumber;
+var confirmSpecial;
+var userSelection;
+
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
 // Write password to the #password input
 function writePassword() {
@@ -24,3 +36,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); 
+
+//Start fumctions
